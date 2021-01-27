3e573f0d3b7e39133a5812a861f73812c934bcb6
MoonClinical <boy.mehdi@live.fr>
2021-01-27 12:45:04 +0000

    updated max length

--- OnlyJobs/django_backend/models.py
+++ OnlyJobs/django_backend/models.py
@@ -3,20 +3,16 @@ from django.db import models
 # Create your models here.
 
 class JobListing(models.Model):
-    company = models.CharField(max_length= 50 , default= None)
-    job_title = models.CharField(max_length= 50 , default= None)
+    company = models.CharField(max_length= 200 , default= None)
+    job_title = models.CharField(max_length= 200 , default= None)
     job_salary = models.PositiveIntegerField( default= None)
-    work_schedule = models.CharField(max_length= 10,  default= None)
-    technologies = models.CharField(max_length= 50 ,  default= None)
-    date_posted = models.CharField(max_length= 50 ,   default= None)
-
-    job_deadline = models.CharField(max_length= 50 ,   default= None)
-
+    work_schedule = models.CharField(max_length= 200,  default= None)
+    technologies = models.CharField(max_length= 200 ,  default= None)
+    date_posted = models.CharField(max_length= 200 ,   default= None)
+    job_deadline = models.CharField(max_length= 200 ,   default= None)
     logo_url = models.URLField(  default= None)
-    job_description = models.CharField(max_length= 50,   default= None)
-
-    job_location = models.CharField(max_length= 50, default= None)
-
+    job_description = models.CharField(max_length= 2000,   default= None)
+    job_location = models.CharField(max_length= 200, default= None)
     job_url_1 = models.URLField(default= None)
     job_url_2 = models.URLField(default= None)
     job_url_3 = models.URLField(default= None)
